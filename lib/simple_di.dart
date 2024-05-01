0b336cd4fb570352897bde281babe93f71cd881c
Dominik Gumiński <drgt344@hotmail.com>
2024-05-01 20:18:37 +0200

    added disposable and error types to exported modules

--- lib/simple_di.dart
+++ lib/simple_di.dart
@@ -4,7 +4,10 @@
 library;
 
 export 'src/service_container_builder.dart';
-export 'src/service_container.dart';
+export 'src/abstraction/service_container.dart';
 export 'src/service_lifetime.dart';
+export 'src/abstraction/disposable.dart';
+export 'src/errors/container_sealed.dart';
+export 'src/errors/service_not_registered.dart';
 
 // TODO: Export any libraries intended for clients of this package.
